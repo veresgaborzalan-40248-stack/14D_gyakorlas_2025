135c2047c0f3c52d48aa1da5fef1888c68ff851c
Programozó <programozo@taszi.lan>
2025-11-19 08:53:50 +0100

    Kezdő WPF projekt

---
+++ WpfGitDemo/WpfGitDemo/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfGitDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
